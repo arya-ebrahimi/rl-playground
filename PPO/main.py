c51a114f7000903ba452f297dded745387e875f0
Arya-Ebrahimi <aryaebrahimi3000@mail.um.ac.ir>
2023-09-24 23:02:47 +0330

    add models trained for 3,000,000 timesteps

--- PPO/main.py
+++ PPO/main.py
@@ -8,12 +8,17 @@ from core.agent import PPO
 def train(env, conf):
     model = PPO(env=env, conf=conf)
 
+    if conf['actor_model'] != None and conf['critic_model'] != None:
+        model.actor.load_state_dict(torch.load(conf['actor_model']))
+        model.critic.load_state_dict(torch.load(conf['critic_model']))
+        print(f"Successfully loaded.", flush=True)
+    
     model.learn(total_timesteps=20000000)
 
 @hydra.main(config_path="config", config_name="config.yaml", version_base=None)
 def main(args):
-    
-    env = gym.make('Pendulum-v1', render_mode='rgb_array')
+        
+    env = gym.make('Pendulum-v1', render_mode='human')
 
     train(env, conf=args)
 
